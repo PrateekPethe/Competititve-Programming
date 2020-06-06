cfc4dce6913e56b3b3d09a9a5ba77586bd921723
PrateekPethe <46239017+PrateekPethe@users.noreply.github.com>
2020-06-06 12:44:17 +0530

    Create XYSTR.cpp

---
+++ CodeChef/June20B/XYSTR.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#define rli long long int
+#define MOD 1000000007
+#define INF 10000000000000
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	if (t>=1 && t<=100)
+	{
+	    while(t--)
+	    {
+	        string s;
+	        cin>>s;
+	        int counter = 0;
+	        int total =0;
+	        for(int i=0;i<s.length()-1;i++)
+	        {
+	            if (s[i]=='x' && s[i+1]=='y')
+	            {
+	                counter++;
+	                i++;
+	            }
+	            else if(s[i]=='y' && s[i+1]=='x')
+	            {
+	                counter++;
+	                i++;
+	            }
+	        }
+	        cout<<counter<<endl;
+	    }
+	}
+	else
+	{
+	    return 0;
+	}
+}
